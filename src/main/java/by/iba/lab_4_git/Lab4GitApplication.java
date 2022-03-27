ff4ba909793a0bc6d305b449b277ea6ed8419308
Siarhei Liucheuski <60502148+liucheuski@users.noreply.github.com>
2022-03-27 09:22:13 +0300

    Initial commit

---
+++ src/main/java/by/iba/lab_4_git/Lab4GitApplication.java
@@ -0,0 +1,13 @@
+package by.iba.lab_4_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4GitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/iba/lab_4_git/Lab4GitApplicationTests.java
@@ -0,0 +1,13 @@
+package by.iba.lab_4_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4GitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
